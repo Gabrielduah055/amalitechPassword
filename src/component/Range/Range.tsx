59667112b945cf9cbfc8151869b665432b0483b4
Gabriel Duah <gabriel.duah@amalitech.com>
2023-12-18 14:01:12 +0100

    converted the password range and number to styling to app component

--- src/component/Range/Range.tsx
+++ src/component/Range/Range.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent } from 'react'
 import './range.css'
-
+import styled from 'styled-components'
 
 type PasswordLengthProps = {
   passwordLength:number
@@ -29,19 +29,69 @@ const Range = ({passwordLength, setPasswordLength} :PasswordLengthProps)  => {
 
   
   return (
-    <div className='rangeContainer'>
-      <div className="charLength">
-        <p>Character Length</p>
-        <p className='length'>{passwordLength}</p>
-      </div>
+    <Container>
+      <CharLength>
+        <CharacterLength>Character Length</CharacterLength>
+        <Length>{passwordLength}</Length>
+      </CharLength>
       <input type="range" 
               min="0" 
               max="10" 
               id='passwordLength'
               value={passwordLength}
               onChange={handlePasswordLength}/>
-    </div>
+    </Container>
   )
 }
 
 export default Range
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: .8rem;
+  margin-bottom: 1.5rem;
+
+  input[type="range"] {
+    -webkit-appearance: none;
+    accent-color: white;
+    cursor: pointer;
+    height: 8px;
+    background-color: var(--color-dark);
+    background-image: linear-gradient(var(--primary-color), var(--primary-color));
+    background-size: 30% 100%;
+    background-repeat: no-repeat;
+}
+
+input[type="range"]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    height: 20px;
+    width: 20px;
+    background-color: white;
+    border-radius: 50%;
+}
+
+input[type="range"]:hover::-webkit-slider-thumb {
+    background: var(--color-dark);
+    border: 2px solid var(--primary-color);
+    -webkit-appearance: none;
+  }
+`
+const CharacterLength = styled.p`
+
+`
+const CharLength = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: var(--color-password);
+  margin-block: .5rem;
+`
+const Length = styled.p`
+
+  color: var(--primary-color);
+  font-weight: 500;
+  font-size: 1.5rem;
+    
+    `
