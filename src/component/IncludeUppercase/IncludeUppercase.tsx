7c4eef369ec92f79eb8cb906b0dd725a1d8f099c
Gabriel Duah <gabriel.duah@amalitech.com>
2023-12-18 13:42:58 +0100

    converted the lowercase check to styling to app component

--- src/component/IncludeUppercase/IncludeUppercase.tsx
+++ src/component/IncludeUppercase/IncludeUppercase.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent } from 'react'
 import './includeUppercase.css'
+import styled from 'styled-components'
 
 
 type UpperCaseProps = {
@@ -17,14 +18,57 @@ const IncludeUppercase = ({includeUppercase, setIncludeUppercase}:UpperCaseProps
   }
 
   return (
-    <div className='upperCaseContainer'>
-      <input type="checkbox" 
-             id='includeUppercase'
-             checked = {includeUppercase}
-             onChange={checkedHandledUppercase}/>
-      <label htmlFor="includeUppercase">Include Uppercase Letters</label>
-    </div>
+    <Container>
+        <UpperCaseContainer>
+          <input type="checkbox" 
+                id='includeUppercase'
+                checked = {includeUppercase}
+                onChange={checkedHandledUppercase}/>
+          <Label htmlFor="includeUppercase">Include Uppercase Letters</Label>
+      </UpperCaseContainer>
+    </Container>
+    
   )
 }
 
 export default IncludeUppercase
+
+
+const Container = styled.div`
+  margin-block:.5rem;
+`
+const UpperCaseContainer = styled.div`
+  display: flex;
+  gap: .5rem;
+  margin-block: .6rem
+
+  input {
+    outline: none;
+    border: none;
+    background-color: var(--primary-color);
+    cursor: pointer;
+  }
+
+  input[type='checkbox'] {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    background-color: var(--color-dark);
+    cursor: pointer;
+    border: 2px solid var(--primary-color);
+  }
+
+  input[type='checkbox']:checked {
+    background-color: var(--primary-color);
+    background-image: url('/src/assets/icon-check.svg');
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+`
+
+
+
+const Label = styled.label`
+  color: var(--color-password);
+`
